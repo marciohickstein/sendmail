99333905e3c2f126bb1b05a918c797798025c272
Marcio Hickstein <marcio.hickstein@gmail.com>
2019-04-11 21:39:22 +0000

    Adicionado o AngularJS no projeto

--- localstorage.js
+++ localstorage.js
@@ -1,19 +1,9 @@
-
 function localStorageAvailable(){
     return (typeof localStorage !== 'undefined');
-}
-
-function readLocalStorage(){
-    if (localStorageAvailable()){
-        document.getElementById("host").value = localStorage.getItem("host");
-        document.getElementById("porta").value = localStorage.getItem("porta");
-        document.getElementById("usuario").value = localStorage.getItem("usuario");
-        document.getElementById("senha").value = localStorage.getItem("senha");
-    }
 }
 
 function writeLocalStorage(key, value){
     if (localStorageAvailable()){
         localStorage.setItem(key, value);
     }
-}
+}        
